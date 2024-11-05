6787eb8585a4f051dbeef94076491f1ded603af0
X3TSr <kylian1202@gmail.com>
2024-11-05 12:06:36 +0100

    added to highlights section

--- src/Pages/Home/Sections/Highlights/Highlights.jsx
+++ src/Pages/Home/Sections/Highlights/Highlights.jsx
@@ -4,30 +4,24 @@ import style from './Highlights.module.css'
 const Highlights = () => {
     return (
         <section>
-            <h2 className={`${style.title}`}>Highlights</h2>
-            <div className={`${style.highlights}`}>
-                <div className={`${style.highlight}`}>
-                    <h3>Webdevelopment</h3>
-                    <p>
-                        I am a webdeveloper with a passion for creating beautiful and functional websites.
+            <h1 className={`${style.title}`}>Higlights</h1>
+            <div className={`${style.highlightsContainer}`}>
+                <div className={`${style.highlightItem}`}>
+                    <h3 className={`${style.highlightTitle}`}>Toki</h3>
+                    <p className={`${style.highlightDescription}`}>
+                        Korean streetfood restaurant located in the heart of Aalst.
                     </p>
                 </div>
-                <div className={`${style.highlight}`}>
-                    <h3>Webdesign</h3>
-                    <p>
+                <div className={`${style.highlightItem}`}>
+                    <h3 className={`${style.highlightTitle}`}>Webdesign</h3>
+                    <p className={`${style.highlightDescription}`}>
                         I am a webdesigner with a passion for creating beautiful and functional websites.
                     </p>
                 </div>
-                <div className={`${style.highlight}`}>
-                    <h3>Student</h3>
-                    <p>
+                <div className={`${style.highlightItem}`}>
+                    <h3 className={`${style.highlightTitle}`}>Student</h3>
+                    <p className={`${style.highlightDescription}`}>
                         I am a student with a passion for learning new things and improving my skills.
-                    </p>
-                </div>
-                <div className={`${style.highlight}`}>
-                    <h3>Creative</h3>
-                    <p>
-                        I am a creative person with a passion for creating new things and solving problems.
                     </p>
                 </div>
             </div>
