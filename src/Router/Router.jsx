8926ba8af807d86a136a0a4d4a7a9be08eae635a
X3TSr <kylian1202@gmail.com>
2024-11-06 10:52:41 +0100

    Fixed routing to not found

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,7 +4,7 @@ import Projects from '../Pages/Projects/Projects';
 import Detail from '../Pages/Detail/Detail';
 import Timeline from '../Pages/Timeline/Timeline';
 import Contact from '../Pages/Contact/Contact';
-import Notfound from '../Pages/Not Found/Notfound';
+import NotFound from '../Pages/Not Found/NotFound';
 import ROUTES from '../consts/ROUTES';
 
 function Router() {
@@ -15,7 +15,7 @@ function Router() {
             <Route path={ROUTES.projectDetail.path} element={<Detail />} />
             <Route path={ROUTES.timeline} element={<Timeline />} />
             <Route path={ROUTES.contact} element={<Contact />} />
-            <Route path={ROUTES.notfound} element={<Notfound />} />
+            <Route path={ROUTES.notfound} element={<NotFound />} />
         </Routes>
     );
 }
