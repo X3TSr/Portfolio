69ff3ea86550ead38984f26b08a92f256adabcb0
X3TSr <kylian1202@gmail.com>
2024-11-06 10:47:46 +0100

    Added not found page

--- src/Pages/Not Found/NotFound.jsx
+++ src/Pages/Not Found/NotFound.jsx
@@ -3,9 +3,10 @@ import style from './NotFound.module.css'
 
 const NotFound = () => {
     return (
-        <div>
-
-        </div>
+        <section className={`${style.sectionNotFound}`}>
+            <h1 className={`${style.title}`}>To be or not to be?</h1>
+            <h2 className={`${style.subtitle}`}>But you aprea to be.. lost</h2>
+        </section>
     );
 };
 
