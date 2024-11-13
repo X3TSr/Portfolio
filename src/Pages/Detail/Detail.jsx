9a57b8bddb1d09b85d2e7bb664e3521889149d3d
X3TSr <kylian1202@gmail.com>
2024-11-13 10:19:08 +0100

    set the title in PROJECTS.json to lowercase

--- src/Pages/Detail/Detail.jsx
+++ src/Pages/Detail/Detail.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from './Detail.module.css'
 import { useParams } from 'react-router-dom';
+import PROJECTS from '../../consts/PROJECTS.json'
 
 const Detail = () => {
 
@@ -9,6 +10,7 @@ const Detail = () => {
     return (
         <section className={`${style.sectionDetail}`}>
             <h1 className={`${style.title}`}>Project: <span className={`${style.dark}`}>{`${title[0].toUpperCase()}${title.slice(1)}`}</span></h1>
+            <p>{PROJECTS[title.toLowerCase()].description}</p>
         </section>
     );
 };
