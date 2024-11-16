77aa822705c6b4a2ef65237fefe1b6203368e7b9
X3TSr <kylian1202@gmail.com>
2024-11-16 18:50:28 +0100

    added ability to visit a project if it is still active

--- src/Pages/Detail/Detail.jsx
+++ src/Pages/Detail/Detail.jsx
@@ -6,14 +6,16 @@ import PROJECTS from '../../consts/PROJECTS.json'
 const Detail = () => {
 
     const { title } = useParams();
+    const project = PROJECTS[title.toLowerCase()];
 
     return (
         <section className={`${style.sectionDetail}`}>
             <h1 className={`${style.title}`}>Project: <span className={`${style.dark}`}>{`${title[0].toUpperCase()}${title.slice(1)}`}</span></h1>
-            <p className={`${style.description}`}>{PROJECTS[title.toLowerCase()].description}</p>
+            <p className={`${style.description}`}>{project.description}</p>
+            <a href={`${project.active ? project.link : ''}`} className={`${style.projectLink}`}>Visit '{project.title}'</a>
             <div className={`${style.images}`}>
                 {
-                    PROJECTS[title.toLowerCase()].images.map((image, index) => (
+                    project.images.map((image, index) => (
                         <img key={index} src={image} alt={title} className={`${style.image}`} />
                     ))
                 }
