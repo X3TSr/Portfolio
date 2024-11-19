4c9ab571352fc67722d9d5925386595fe79001dd
X3TSr <kylian1202@gmail.com>
2024-11-19 08:06:44 +0100

    Hero reset to black bg

--- src/Components/HeroContainer/HeroContainer.jsx
+++ src/Components/HeroContainer/HeroContainer.jsx
@@ -3,10 +3,8 @@ import style from './HeroContainer.module.css'
 
 const HeroContainer = () => {
     return (
-        <div className={`${style.heroSContainer}`}>
-            <div className={`${style.heroS} ${style.heroS1}`}></div>
-            <div className={`${style.heroS} ${style.heroS2}`}></div>
-            <div className={`${style.heroS} ${style.heroS3}`}></div>
+        <div className={`${style.heroContainer}`}>
+
         </div>
     );
 };
