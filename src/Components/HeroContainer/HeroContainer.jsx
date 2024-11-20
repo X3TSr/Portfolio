63d9a26cf52bb9c573ebdb7fb2796851688e5bf1
X3TSr <kylian1202@gmail.com>
2024-11-20 18:06:35 +0100

    Added a 3d Hero and options for eventual other accent colors

--- src/Components/HeroContainer/HeroContainer.jsx
+++ src/Components/HeroContainer/HeroContainer.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import style from './HeroContainer.module.css'
+import { Canvas } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
 
 const HeroContainer = () => {
+
+    const obj = useGLTF('/3dModels/LowPollyTree.glb');
+    const meshRef = useRef();
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (meshRef.current) {
+                meshRef.current.rotation.y = window.scrollY * 0.0005;
+                meshRef.current.rotation.z = window.scrollY * 0.0005;
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div className={`${style.heroContainer}`}>
-
+            <Canvas
+                shadows
+                camera={{
+                    fov: 45,
+                    near: 0.1,
+                    far: 200,
+                    position: [50, 60, 0]
+                }}
+            >
+                <ambientLight intensity={0} />
+                <directionalLight position={[50, 60, 5]} intensity={10} color={'#6e5a99'} />
+                <primitive ref={meshRef} object={obj.scene} />
+            </Canvas>
         </div>
     );
 };
